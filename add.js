9bba7a99d64b0cb005cbc934607451a9102d8172
adivar1 <Aditya.Varal@exfo.com>
2024-08-16 18:46:53 +0530

    task 1.7 : initial sum implmentation.

--- add.js
+++ add.js
@@ -1,10 +1,17 @@
 function add(numbers){
-
+    let sum = 0;
     if(!numbers){
         return 0;
-    }
+    } else {
+        let nums = numbers.split(',');
+        sum = nums.reduce((acc,elem)=>{
+            return acc = acc + parseInt(elem);
+        },0);
 
-    return 0;
+        return sum;
+    }
 }
+
+console.log(add('3,5'));
 
 module.exports = add;
