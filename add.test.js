f6562dcb9d7a1aec28c6649680981156faf43dc3
adivar1 <Aditya.Varal@exfo.com>
2024-08-16 18:05:46 +0530

    Task 1.3: -> Test case for single input and multiple input added.

--- add.test.js
+++ add.test.js
@@ -4,4 +4,13 @@ describe('String Calculator', () => {
     test('should return 0 for an empty string', () => {
         expect(add("")).toBe(0);
     });
+
+    test('should return input number if there is only one input',()=> {
+        expect(add("1").tobe(1));
+    });
+
+    test('should accept multiple numbers as input',()=> {
+            expect(add("1,4,6").tobe(11));
+    });
+
 });
